266b945f0f063cc32db37ff0ca3dd6550041e4e1
sorbonaANOTHERDAMNWSL <reeldob34.com>
2024-10-07 21:35:58 +0800

    adasdasd

--- attacks.js
+++ attacks.js
@@ -136,11 +136,11 @@ const initAttacks = (main) => {
   // * Init
   let currentTime = 3 * 60;
 
-  // // * Main attacks
-  // currentTime = thatoneannoyingstarterattack(main, currentTime);
+  // * Main attacks
+  currentTime = thatoneannoyingstarterattack(main, currentTime);
 
-  // currentTime += 120;
-  // currentTime = PILLARJHONSREVENGE(main, currentTime);
+  currentTime += 120;
+  currentTime = PILLARJHONSREVENGE(main, currentTime);
 
   currentTime += 120;
   currentTime = spiralHellstorm(main, currentTime);
